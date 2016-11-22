5a93f4fa11cbb603371754cdc1cda79aef38e3c9
unknown <alumno@LAB02PC10.labs.extension>
2016-11-22 11:28:11 -0300

    Console application eventos.

---
+++ UltimaClaseEventos/Eventos/ConsoleEventsProject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleEventsProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            miClase personita = new miClase();
+         
+            personita.noEsLaEdad += Program.juna;
+
+            personita.esLaEdad(10);
+
+            Console.ReadKey();
+        }
+
+        public static void juna(int edad)
+        {
+            Console.WriteLine("La edad no coincide.");
+        }
+    }
+}
---
+++ UltimaClaseEventos/Eventos/ConsoleEventsProject/miClase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleEventsProject
+{
+    public delegate void miDelegado(int edad);
+    public class miClase
+    {
+        public int edad;
+
+        public void esLaEdad(int edad)
+        {
+            if (this.edad != edad)
+            {
+                noEsLaEdad(edad);
+            }
+        }
+
+        public event miDelegado noEsLaEdad;
+    }
+}
